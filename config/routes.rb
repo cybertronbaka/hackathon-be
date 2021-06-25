c164a07d37ac92f0539fc3f94e2566a3ac27bb48
cybertronbaka <dg.dorjigyeltshen@gmail.com>
2021-06-25 18:05:18 +0600

    Change route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
-    resources :detections, only: %i[index]
+    post 'detect', to: 'detections#index'
   end
 end
