94f8d71d262d8b1d0392fc8e98720568e4c37829
cybertronbaka <dg.dorjigyeltshen@gmail.com>
2021-06-25 17:58:51 +0600

    Fix

--- app/controllers/api/detections_controller.rb
+++ app/controllers/api/detections_controller.rb
@@ -1,9 +1,9 @@
 module Api
   class DetectionsControllers < ApplicationController
     def index
-      photo = UploadPhotoService.new(params: params).run
+      photo = UploadPhotoService.new(params: params, file: params[:image]).run
       output = AiService.new(path: photo.path).run
-      result = AiLabelParse.new(output: output).run
+      result = AiLabelParser.new(output: output).run
       #Need to add all the solutions and disease descriptions
       render json: result
     end
